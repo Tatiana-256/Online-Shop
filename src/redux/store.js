a373409173b0bc7b29893cfe69e91ccff272fc22
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-03-12 20:19:55 +0200

    Changes in store.js. deploy project on heroky. Address: https://your-online-shop-ua.herokuapp.com/

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,11 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middleWares = [logger];
+const middleWares = [];
 
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export const persistor = persistStore(store);
